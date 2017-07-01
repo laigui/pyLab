7e26f3dd13791025745929608dbfcb677ea3445a
laigui <laigui@gmail.com>
2017-07-01 10:02:16 +0800

    add hex conversion support

--- uart_rx_timestamp.py
+++ uart_rx_timestamp.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 import serial
+import binascii
 from time import sleep
 from time import localtime, strftime
 
-ser = serial.Serial(port="/dev/tty.SLAB_USBtoUART", baudrate=9600, timeout=10)
+ser = serial.Serial(
+    port='/dev/tty.SLAB_USBtoUART',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS
+)
 
 print('serial test start ...')
 rx_cnt = 0
@@ -12,7 +19,7 @@ try:
     while True:
         if ser.in_waiting > 0:
             current_time = strftime("test-%y%m%d-%H:%M:%S  ", localtime())
-            print current_time + ser.read(ser.in_waiting) + '\n'
+            print current_time + binascii.b2a_hex(ser.read(ser.in_waiting)) + '\n'
             sleep(1)
 except KeyboardInterrupt:
     if ser != None:
